fb549c49ed5ef3e017b31f7eb81ee8236e3fb25f
Your Name <you@example.com>
2018-01-06 11:23:48 +0700

    added: security tag, ignoring resource, custom error-page, ..

--- src/main/java/com/kshrd/springbootdemo/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/com/kshrd/springbootdemo/configuration/security/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.kshrd.springbootdemo.configuration.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -35,6 +36,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 		
 		//authorize url
 		http.authorizeRequests()
+			.antMatchers("/user/add").hasRole("ADMIN")
+			.antMatchers("/user/edit**").hasRole("ADMIN")
 			.anyRequest().authenticated();
 	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+		   .antMatchers("/assets/**")
+		   .antMatchers("/webjars/**");
+	}
+	
 }
