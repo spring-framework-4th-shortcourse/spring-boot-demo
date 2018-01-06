c31296e1c9d58c87cce98a8db33fd9da1a6b5a55
Phearun Rath <rathphearun168@gmail.com>
2018-01-06 09:20:32 +0700

    added: thymeleaf security tag

---
+++ src/main/java/com/kshrd/springbootdemo/configuration/security/WebSecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.kshrd.springbootdemo.configuration.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()	
+			.withUser("user").password("pwd").roles("USER").and()
+			.withUser("dba").password("pwd").roles("DBA").and()
+			.withUser("admin").password("pwd").roles("ADMIN");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//http.csrf().disable();
+		
+		//enable form login
+		http.formLogin()
+			//custom login page
+			.loginPage("/login").permitAll();
+		
+		//logout
+		http.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/");
+		
+		//authorize url
+		http.authorizeRequests()
+			.anyRequest().authenticated();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/assets/**")
+			.antMatchers("/webjars/**");
+	}
+	
+}
