728f3aced00e5f1606b042d45ade4dffaee1e03b
Your Name <you@example.com>
2018-01-06 09:33:52 +0700

    added: login

--- src/main/java/com/kshrd/springbootdemo/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/com/kshrd/springbootdemo/configuration/security/WebSecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.kshrd.springbootdemo.configuration.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -38,12 +37,4 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 			.anyRequest().authenticated();
 	}
-	
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring()
-			.antMatchers("/assets/**")
-			.antMatchers("/webjars/**");
-	}
-	
 }
